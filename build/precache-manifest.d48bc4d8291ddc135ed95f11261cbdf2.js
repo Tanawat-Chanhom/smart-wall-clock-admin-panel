9c23e7df4c6836d70c27125f5f9f3aa5224d10a7
Tanawat <61070078@kmitl.ac.th>
2020-02-01 01:06:35 +0700

    Init

---
+++ build/precache-manifest.d48bc4d8291ddc135ed95f11261cbdf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e4eb32df22d5b7c36096487331ca28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7caa3d3767122c41889b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0ba334f729425ec4a34a",
+    "url": "/static/js/2.624e1675.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.624e1675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7caa3d3767122c41889b",
+    "url": "/static/js/main.f2cf2589.chunk.js"
+  },
+  {
+    "revision": "d644ff198816bb9f0669",
+    "url": "/static/js/runtime-main.c0627188.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
